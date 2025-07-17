20867ab7796a9f6c2860fed6e3cfa8944fa99441
Taeho Seong <152877524+racoonSing@users.noreply.github.com>
2025-07-17 14:26:31 +0900

    Update server.py

--- server.py
+++ server.py
@@ -10,46 +10,43 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
-@app.route("/detect", methods=["POST"])
+@app.route("/compare", methods={"POST"})
+def compare():
+    try:
+        if request.method == "POST":
+            f1 = request.files["file1"]
+            f2 = request.files["file2"]
+
+            f1_filename = secure_filename(f1.filename)
+            f2_filename = secure_filename(f2.filename)
+
+            f1.save("static/" + f1_filename)
+            f2.save("static/" + f2_filename)
+
+            r = compare_faces("static/" + f1_filename, "static/" + f2_filename)
+
+            return r
+            
+    except:
+        return "감지 실패"
+
+@app.route("/detect", methods={"POST"})
 def detect():
     try:
         if request.method == "POST":
             f = request.files["file"]
 
             filename = secure_filename(f.filename)
-
             # 외부에서 온 이미지, 파일 등을
             # 마음대로 저장할 수 없음
-            
+            # 서버에 클라이언트가 보낸 이미지를 저장
             f.save("static/" + filename)
             r = label("static/" + filename)
             return r
-            # 서버에 클라이언트가 보낸 이미지를 저장!!
             
     except:
         return "감지 실패"
-    
-@app.route("/compare", methods=["POST"])
-def compare():
-    try:
-        if request.method == "POST":
-            c_1 = request.files["image_1"]
-            c_2 = request.files["image_2"]
 
-            imagename_1 = secure_filename(c_1.filename)
-            imagename_2 = secure_filename(c_2.filename)
-
-            c_1.save("static/" + imagename_1)
-            c_2.save("static/" + imagename_2)
-
-            r = compare_faces("static/" + imagename_1, "static/" + imagename_2)
-            return r
-
-    except:
-        return "사진 감지 실패"
-    
-    return "얼굴 비교 페이지"
- 
 @app.route("/mbti", methods=["POST"])
 def mbti():
     try:
@@ -58,21 +55,20 @@ def mbti():
 
             return f"당신의 MBTI는 {mbti}입니다"
     except:
-        return "데이터 수신 실패" 
+        return "데이터 수신 실패"
 
 @app.route("/login", methods=["GET"])
 def login():
     try:
         if request.method == "GET":
-            # login_id, login_pw
             # get -> request.args
             login_id = request.args["login_id"]
             login_pw = request.args["login_pw"]
 
-            if (login_id == "taeho") and (login_pw == "1234"):
+            if (login_id == "jiin") and (login_pw == "1234"):
                 # 로그인 성공 -> 로그인 성공 페이지로 이동
-                # taeho님 환영합니다.
-
+                # nayeho님 환영합니다
+                
                 response = make_response(redirect("/login/success"))
                 response.set_cookie("user", login_id)
 
@@ -82,6 +78,7 @@ def login():
                 return redirect("/")
     except:
         return "로그인 실패"
+    
 @app.route("/login/success", methods=["GET"])
 def login_success():
 
